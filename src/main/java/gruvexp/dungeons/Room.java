3a7df8b999b4203aac83920cdd3a3034607c1a23
GruveXp <gruveexp123@gmail.com>
2025-01-28 22:07:31 +0100

    rooms can now support being saved in different folders

--- src/main/java/gruvexp/dungeons/Room.java
+++ src/main/java/gruvexp/dungeons/Room.java
@@ -2,25 +2,25 @@ package gruvexp.dungeons;
 
 public enum Room {
     // dungeon
-    END(new DungeonStructure("end", new Coord(2, 1, 0)), GrowRate.SHRINKING, RoomType.CATACOMB_WALKWAY),
-    STRAIGHT(new DungeonStructure("straight", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.CATACOMB_WALKWAY),
-    SKELETON_TRAP(new DungeonStructure("skeleton_trap", new Coord(3, 1, 0)), GrowRate.STATIC, RoomType.CATACOMB_WALKWAY), // +1x pga må ha en blokk for å henge tripwire hook på
-    ARROW_TRAP(new DungeonStructure("arrow_trap", new Coord(3, 1, 0)), GrowRate.STATIC, RoomType.CATACOMB_WALKWAY),
-    TURN_R(new DungeonStructure("turn_r", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.CATACOMB_WALKWAY),
-    TURN_L(new DungeonStructure("turn_l", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.CATACOMB_WALKWAY),
-    T_SPLIT_R(new DungeonStructure("t_split_r", new Coord(2, 1, 0)), GrowRate.EXPANDING, RoomType.CATACOMB_WALKWAY),
-    T_SPLIT_M(new DungeonStructure("t_split_m", new Coord(2, 1, 0)), GrowRate.EXPANDING, RoomType.CATACOMB_WALKWAY),
-    T_SPLIT_L(new DungeonStructure("t_split_l", new Coord(2, 1, 0)), GrowRate.EXPANDING, RoomType.CATACOMB_WALKWAY),
+    END(new DungeonStructure("dungeon", "end", new Coord(2, 1, 0)), GrowRate.SHRINKING, RoomType.CATACOMB_WALKWAY),
+    STRAIGHT(new DungeonStructure("dungeon", "straight", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.CATACOMB_WALKWAY),
+    SKELETON_TRAP(new DungeonStructure("dungeon", "skeleton_trap", new Coord(3, 1, 0)), GrowRate.STATIC, RoomType.CATACOMB_WALKWAY), // +1x pga må ha en blokk for å henge tripwire hook på
+    ARROW_TRAP(new DungeonStructure("dungeon", "arrow_trap", new Coord(3, 1, 0)), GrowRate.STATIC, RoomType.CATACOMB_WALKWAY),
+    TURN_R(new DungeonStructure("dungeon", "turn_r", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.CATACOMB_WALKWAY),
+    TURN_L(new DungeonStructure("dungeon", "turn_l", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.CATACOMB_WALKWAY),
+    T_SPLIT_R(new DungeonStructure("dungeon", "t_split_r", new Coord(2, 1, 0)), GrowRate.EXPANDING, RoomType.CATACOMB_WALKWAY),
+    T_SPLIT_M(new DungeonStructure("dungeon", "t_split_m", new Coord(2, 1, 0)), GrowRate.EXPANDING, RoomType.CATACOMB_WALKWAY),
+    T_SPLIT_L(new DungeonStructure("dungeon", "t_split_l", new Coord(2, 1, 0)), GrowRate.EXPANDING, RoomType.CATACOMB_WALKWAY),
 
     // fortress
-    FORTRESS_BRIDGE_STRAIGHT(new DungeonStructure("fortress/bridge_straight", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.FORTRESS_BRIDGE),
-    FORTRESS_BRIDGE_STRAIGHT_POLE(new DungeonStructure("fortress/bridge_straight_pole", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.FORTRESS_BRIDGE),
-    FORTRESS_BRIDGE_END(new DungeonStructure("fortress/bridge_end", new Coord(2, 1, 0)), GrowRate.SHRINKING, RoomType.FORTRESS_BRIDGE),
-    FORTRESS_BRIDGE_INTER_OPEN(new DungeonStructure("fortress/bridge_t", new Coord(10, 1, 0)), GrowRate.EXPANDING, RoomType.FORTRESS_BRIDGE),
-    FORTRESS_BRIDGE_INTER_CLOSED(new DungeonStructure("fortress/bridge_x", new Coord(2, 1, 0)), GrowRate.EXPANDING, RoomType.FORTRESS_BRIDGE),
-    FORTRESS_BRIDGE_BLAZE(new DungeonStructure("fortress/bridge_blaze", new Coord(2, 6, 0)), GrowRate.STATIC, RoomType.FORTRESS_BRIDGE),
-    FORTRESS_BRIDGE_L(new DungeonStructure("fortress/bridge_l", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.FORTRESS_BRIDGE),
-    FORTRESS_BRIDGE_R(new DungeonStructure("fortress/bridge_r", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.FORTRESS_BRIDGE);
+    FORTRESS_BRIDGE_STRAIGHT(new DungeonStructure("fortress", "bridge_straight", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.FORTRESS_BRIDGE),
+    FORTRESS_BRIDGE_STRAIGHT_POLE(new DungeonStructure("fortress", "bridge_straight_pole", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.FORTRESS_BRIDGE),
+    FORTRESS_BRIDGE_END(new DungeonStructure("fortress", "bridge_end", new Coord(2, 1, 0)), GrowRate.SHRINKING, RoomType.FORTRESS_BRIDGE),
+    FORTRESS_BRIDGE_INTER_OPEN(new DungeonStructure("fortress", "bridge_t", new Coord(10, 1, 0)), GrowRate.EXPANDING, RoomType.FORTRESS_BRIDGE),
+    FORTRESS_BRIDGE_INTER_CLOSED(new DungeonStructure("fortress", "bridge_x", new Coord(2, 1, 0)), GrowRate.EXPANDING, RoomType.FORTRESS_BRIDGE),
+    FORTRESS_BRIDGE_BLAZE(new DungeonStructure("fortress", "bridge_blaze", new Coord(2, 6, 0)), GrowRate.STATIC, RoomType.FORTRESS_BRIDGE),
+    FORTRESS_BRIDGE_L(new DungeonStructure("fortress", "bridge_l", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.FORTRESS_BRIDGE),
+    FORTRESS_BRIDGE_R(new DungeonStructure("fortress", "bridge_r", new Coord(2, 1, 0)), GrowRate.STATIC, RoomType.FORTRESS_BRIDGE);
 
     private final DungeonStructure structure;
     public final GrowRate growRate;
