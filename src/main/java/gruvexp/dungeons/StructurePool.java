d136ed55649f53ba7267ca96aae6aada64c100e5
GruveXp <gruveexp123@gmail.com>
2025-01-28 22:51:08 +0100

    fix weight calculation errors

--- src/main/java/gruvexp/dungeons/StructurePool.java
+++ src/main/java/gruvexp/dungeons/StructurePool.java
@@ -7,29 +7,36 @@ import java.util.Random;
 public class StructurePool {
 
     private final Map<GrowRate, Map<Room, Integer>> structures = new HashMap<>();
-    private int totalWeight = 0;
+    private final Map<GrowRate, Integer> structureWeights = new HashMap<>();
 
     public StructurePool() {
         structures.put(GrowRate.SHRINKING, new HashMap<>());
         structures.put(GrowRate.STATIC, new HashMap<>());
         structures.put(GrowRate.EXPANDING, new HashMap<>());
+        structureWeights.put(GrowRate.SHRINKING, 0);
+        structureWeights.put(GrowRate.STATIC, 0);
+        structureWeights.put(GrowRate.EXPANDING, 0);
     }
 
     public void addStructure(Room room, int weight) {
-        structures.get(room.growRate).put(room, weight);
-        totalWeight += weight;
+        GrowRate growRate = room.growRate;
+        structures.get(growRate).put(room, weight);
+        structureWeights.put(growRate, structureWeights.get(growRate) + weight);
     }
 
     public void updateWeight(Room room, int newWeight) {
-        if (structures.get(room.growRate).containsKey(room)) {
-            totalWeight -= structures.get(room.growRate).get(room);
-            structures.get(room.growRate).put(room, newWeight);
-            totalWeight += newWeight;
+        GrowRate growRate = room.growRate;
+        if (structures.get(growRate).containsKey(room)) {
+            structureWeights.put(growRate, structureWeights.get(growRate) + structures.get(growRate).get(room));
+            structures.get(growRate).put(room, newWeight);
+            structureWeights.put(growRate, structureWeights.get(growRate) + newWeight);
         }
     }
 
     public Room getRandomStructure(GrowRate growRate) {
-        int roll = new Random().nextInt(totalWeight); // random tall mellom 0 og totalWeight
+        //System.out.println("growrate is " + growRate.name());
+        int roll = new Random().nextInt(structureWeights.get(growRate)); // random tall mellom 0 og totalWeight
+        //System.out.println("total weight: " + structureWeights.get(growRate) + ", roll=" + roll);
         int currentWeight = 0;
 
         for (Map.Entry<Room, Integer> entry : structures.get(growRate).entrySet()) {
