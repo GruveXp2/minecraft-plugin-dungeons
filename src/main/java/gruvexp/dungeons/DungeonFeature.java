e36c59b49124ebf92214c9b0da8e2e3adce262c1
GruveXp <gruveexp123@gmail.com>
2025-02-24 20:50:23 +0100

    remove redundant code

--- src/main/java/gruvexp/dungeons/DungeonFeature.java
+++ src/main/java/gruvexp/dungeons/DungeonFeature.java
@@ -5,7 +5,6 @@ import gruvexp.dungeons.location.Direction;
 import org.bukkit.*;
 import org.bukkit.block.structure.Mirror;
 import org.bukkit.block.structure.StructureRotation;
-import org.bukkit.entity.TextDisplay;
 import org.bukkit.structure.Structure;
 
 import java.util.ArrayList;
@@ -24,103 +23,6 @@ public class DungeonFeature {
             }
             STRUCTURES.add(structure);
         }
-    }
-
-    private static String rotateNode(String dir, StructureRotation structureRotation) {
-        dir = toAbsoluteDirection(dir);
-        switch (structureRotation) {
-            case NONE -> {
-                return dir;
-            }
-            case CLOCKWISE_90 -> {
-                return switch (dir) {
-                    case "N" -> "E";
-                    case "E" -> "S";
-                    case "S" -> "W";
-                    case "W" -> "N";
-                    case "NS" -> "EW";
-                    case "EW" -> "NS";
-                    default -> "ERROR";
-                };
-            }
-            case CLOCKWISE_180 -> {
-                return switch (dir) {
-                    case "N" -> "S";
-                    case "E" -> "W";
-                    case "S" -> "N";
-                    case "W" -> "E";
-                    case "NS", "EW" -> dir;
-                    default -> "ERROR";
-                };
-            }
-            case COUNTERCLOCKWISE_90 -> {
-                return switch (dir) {
-                    case "N" -> "W";
-                    case "E" -> "N";
-                    case "S" -> "E";
-                    case "W" -> "S";
-                    case "NS" -> "EW";
-                    case "EW" -> "NS";
-                    default -> "ERROR";
-                };
-            }
-            default -> {
-                return "ERROR DungeonFeature:69";
-            }
-        }
-    }
-
-    private static Location rotateLocation(Location loc, String dir) {
-        switch (dir) {
-            case "S" -> {
-                loc.setX(loc.getBlockX());
-                loc.setZ(loc.getBlockZ());
-            }
-            case "N" -> {
-                loc.setX(-loc.getBlockX());
-                loc.setZ(-loc.getBlockZ());
-            }
-            case "E" -> {
-                double oldX = loc.getBlockX();
-                loc.setX(loc.getBlockZ());
-                loc.setZ(-oldX);
-            }
-            case "W" -> {
-                double oldX = loc.getBlockX();
-                loc.setX(-loc.getBlockZ());
-                loc.setZ(oldX);
-            }
-        }
-        return loc;
-    }
-
-    private static void moveForward(Location loc, Direction dir, int blocks) {
-        switch (dir) {
-            case S -> loc.add(0, 0, blocks);
-            case W -> loc.add(-blocks, 0, 0);
-            case N -> loc.add(0, 0, -blocks);
-            case E -> loc.add(blocks, 0, 0);
-        }
-    }
-
-    private static String toAbsoluteDirection(String relDir) {
-        return switch (relDir) {
-            case "Forward" -> "S";
-            case "Right" -> "W";
-            case "Left" -> "E";
-            case "Backward" -> "N";
-            case "FB" -> "NS";
-            case "RL" -> "EW";
-            default -> "ERROR DungeonFeature:113";
-        };
-    }
-
-    private static void spawnTextMarker(Location loc, String name) {
-        Location spawnLoc = new Location(loc.getWorld(), loc.getX() + 0.5, loc.getY(), loc.getZ() + 0.5);
-        TextDisplay textDisplay = Main.WORLD.spawn(spawnLoc, TextDisplay.class);
-        textDisplay.setText(name);
-        textDisplay.setBillboard(TextDisplay.Billboard.VERTICAL);
-        textDisplay.addScoreboardTag("textmarker");
     }
 
     private void moveToOrigin(Location loc, Direction dir) {
